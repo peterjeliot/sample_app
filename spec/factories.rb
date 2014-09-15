845b5f6a077a3c184a13e63c0d3b460ddcd2db45
doublefelix <peterjeliot@gmail.com>
2014-09-15 13:04:27 -0400

    Pagination for user index

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,8 +1,8 @@
 FactoryGirl.define do
   factory :user do
-    name             "Peter Eliot"
-    email      "peter@example.com"
-    password              "foobar"
-    password_confirmation "foobar"
+    sequence(:name)   { |n| "Person #{n}" }
+    sequence(:email)  { |n| "person_#{n}@example.com" }
+    password 'foobar'
+    password_confirmation 'foobar'
   end
 end
