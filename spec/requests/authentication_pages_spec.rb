1b98ffe44daca583cfa22fa207425f90987e938a
doublefelix <peterjeliot@gmail.com>
2014-09-30 23:13:34 -0700

    Microposts exist, have user dependency

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -66,6 +66,30 @@ describe "AuthenticationPages" do
           it "should render the desired protected page" do
             expect(page).to have_title 'Edit user'
           end
+
+          describe "twice" do
+            let(:params) do
+                { user: { name: user.name, password: user.password } }
+            end
+            before do
+              get signin_path, params
+              expect(page).to have_title user.name
+            end
+          end
+
+          describe "when signing in again" do
+            before do
+              click_link "Sign out"
+              visit signin_path
+              fill_in "Email",    with: user.email
+              fill_in "Password", with: user.password
+              click_button "Sign in"
+            end
+
+            it "should render the default (profile) page" do
+              expect(page).to have_title(user.name)
+            end
+          end
         end
       end
 
