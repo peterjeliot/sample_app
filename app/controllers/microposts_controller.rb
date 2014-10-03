b15b460507af1895e1e596029a39d7d12e78ef54
doublefelix <peterjeliot@gmail.com>
2014-10-02 21:00:54 -0700

    Proto-feed working (10.3.3)

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -7,6 +7,8 @@ class MicropostsController < ApplicationController
       flash[:success] = "Micropost created!"
       redirect_to root_url
     else
+      @feed_items = []
+      # redirect_to root_url
       render 'static_pages/home'
     end
   end
